ea7f2a6f4fdc3c9995e21d01748ffe486748b61a
Glit <vpad.lazar.04@gmail.com>
2022-11-26 15:32:53 +0200

    Add project files.

---
+++ Lab_08_1_String/Lab_08_1_String.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Change(string& s)
+{
+	size_t pos = 0;
+	while ((pos = s.find('G', pos)) != -1)
+		if ((s[pos - 1] && s[pos + 1] == 'O') || (s[pos - 1] && s[pos + 1] == 'A'))
+			s.replace(pos - 1, 3, "**");
+	return s;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
