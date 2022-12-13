3a44026872951568eaa68db48d113417413ae39b
Glit <vpad.lazar.04@gmail.com>
2022-12-13 20:04:50 +0200

    Fix

--- Lab_08_1_String/Lab_08_1_String.cpp
+++ Lab_08_1_String/Lab_08_1_String.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 string Change(string& s)
 {
+	int k = 0;
 	size_t pos = 0;
-	while ((pos = s.find('G', pos)) != -1)
-		if ((s[pos - 1] && s[pos + 1] == 'O') || (s[pos - 1] && s[pos + 1] == 'A'))
+	while ((pos = s.find('G', pos + 1)) != -1)
+		if ((s[pos - 1] == 'O' && s[pos + 1] == 'O') || (s[pos - 1] == 'A' && s[pos + 1] == 'A'))
+		{
+			k++;
 			s.replace(pos - 1, 3, "**");
+		}
+	if (k > 0)
+		cout << "Yes, Count : " << k << endl;
+	else
+		cout << "No" << endl;
 	return s;
 }
 
--- UnitTest_Lab_08_1_String/UnitTest_Lab_08_1_String.cpp
+++ UnitTest_Lab_08_1_String/UnitTest_Lab_08_1_String.cpp
@@ -8,7 +8,7 @@ string TestChange(string& s)
 {
 	size_t pos = 0;
 	while ((pos = s.find('G', pos)) != -1)
-		if ((s[pos - 1] && s[pos + 1] == 'O') || (s[pos - 1] && s[pos + 1] == 'A'))
+		if ((s[pos - 1] == 'O' && s[pos + 1] == 'O') || (s[pos - 1] == 'A' && s[pos + 1] == 'A'))
 			s.replace(pos - 1, 3, "**");
 	return s;
 }
